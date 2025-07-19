233f2cb7aa4c491e1cb6485a10814fee96865814
nikhilc52 <nsc49@cornell.edu>
2025-07-19 17:12:26 -0400

    Added basecode for tooltips

---
+++ website/toolips.js
@@ -0,0 +1,31 @@
+// list of tool tip points
+var lst = ["appearances"];
+var _loop_1 = function (i) {
+    var elem = document.getElementById(lst[i]);
+    elem.addEventListener('mousemove', function (e) { reveal(lst[i], e); });
+    // initially fade out
+    var desc = document.getElementById(lst[i] + "-desc");
+    desc.classList.toggle('fade-opacity');
+    // on in/out, fade the opacity
+    elem.addEventListener('mouseout', function (e) { opacity_animation(lst[i]); });
+    elem.addEventListener('mouseover', function (e) { opacity_animation(lst[i]); });
+};
+// for each of the tooltips, add the reveal function on mouseover and hide on mouse out
+for (var i = 0; i < lst.length; i++) {
+    _loop_1(i);
+}
+function reveal(name, e) {
+    // get the icon that the tooltip is based on and the description
+    var icon = document.getElementById(name);
+    var desc = document.getElementById(name + "-desc");
+    // offset left is the number of pixels from the edges of the square
+    // the subtraction is how far on the client viewport the mouse is vs the icon itself
+    // the calculation offsets the description from the icon by that amount
+    desc.style.left = icon.offsetLeft + (e.clientX - icon.getBoundingClientRect().left) + "px";
+    desc.style.top = icon.offsetTop + (e.clientY - icon.getBoundingClientRect().top) + "px";
+    desc.style.visibility = 'visible';
+}
+function opacity_animation(name) {
+    var desc = document.getElementById(name + "-desc");
+    desc.classList.toggle('fade-opacity');
+}
--- website/toolips.ts
+++ website/toolips.ts
@@ -4,22 +4,32 @@ const lst = ["appearances"]
 // for each of the tooltips, add the reveal function on mouseover and hide on mouse out
 for (let i = 0; i < lst.length; i++) {
     const elem = document.getElementById(lst[i]);
-    elem!.addEventListener('mouseover', function (e) { reveal(lst[i]) });
-    const desc = document.getElementById(lst[i] + "-desc");
+    elem!.addEventListener('mousemove', function (e) { reveal(lst[i], e) });
+    
     // initially fade out
+    const desc = document.getElementById(lst[i] + "-desc");
     desc!.classList.toggle('fade-opacity');
-    elem!.addEventListener('mouseout', function (e) { hide(lst[i]) });
+
+    // on in/out, fade the opacity
+    elem!.addEventListener('mouseout', function (e) { opacity_animation(lst[i]) });
+    elem!.addEventListener('mouseover', function (e) { opacity_animation(lst[i]) });
 }
 
-function reveal(name: string) {
+function reveal(name: string, e : MouseEvent) {
+    // get the icon that the tooltip is based on and the description
+    const icon = document.getElementById(name);
     const desc = document.getElementById(name + "-desc");
-    // desc!.style. = + "px"
+    
+    // offset left is the number of pixels from the edges of the square
+    // the subtraction is how far on the client viewport the mouse is vs the icon itself
+    // the calculation offsets the description from the icon by that amount
+    desc!.style.left = icon!.offsetLeft + (e.clientX - icon!.getBoundingClientRect().left) + "px"
+    desc!.style.top = icon!.offsetTop + (e.clientY - icon!.getBoundingClientRect().top) + "px"
 
     desc!.style.visibility = 'visible'
-    desc!.classList.toggle('fade-opacity');
 }
 
-function hide(name: string) {
+function opacity_animation(name: string) {
     const desc = document.getElementById(name + "-desc");
     desc!.classList.toggle('fade-opacity');
 }
