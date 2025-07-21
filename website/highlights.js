b7c05eae00ea628bca8850b718e0ee244ec4d5f6
nikhilc52 <nsc49@cornell.edu>
2025-07-21 01:50:27 -0400

    Half implemented scrolling, needs testing

--- website/highlights.js
+++ website/highlights.js
@@ -1,7 +1,24 @@
+// adapted from https://www.30secondsofcode.org/js/s/element-is-visible-in-viewport/
+// if the element is fully visible
+function elementIsVisibleInViewport(el) {
+    const { top, left, bottom, right } = el.getBoundingClientRect();
+    const { innerHeight, innerWidth } = window;
+    return top >= 0 && left >= 0 && bottom <= innerHeight && right <= innerWidth;
+};
+
+// if the element is fully hidden
+function elementIsFullyHiddenInViewport(el) {
+    const { top, left, bottom, right } = el.getBoundingClientRect();
+    const { innerHeight, innerWidth } = window;
+    return !((top > 0 && top < innerHeight) ||
+        (bottom > 0 && bottom < innerHeight)) &&
+        ((left > 0 && left < innerWidth) || (right > 0 && right < innerWidth))
+};
+
 // map of highlight elements as keys, values are booleans to see if the highlight is displayed
 const highlight_list = new Map(Array.from(document.getElementsByClassName('highlight')).map(value => [value, false]))
 
-document.addEventListener('wheel', (e) => {
+function check_highlights() {
     for (const key of highlight_list.keys()) {
         // if the element is visible and it is not 
         if (elementIsVisibleInViewport(key) && !(highlight_list.get(key))) {
@@ -16,4 +33,10 @@ document.addEventListener('wheel', (e) => {
             highlight_list.set(key, false)
         }
     }
+}
+
+document.addEventListener('wheel', (e) => {
+    check_highlights()
 });
+
+window.onload = check_highlights()
--- website/scrolling.js
+++ website/scrolling.js
@@ -1,51 +1,169 @@
-// TODO: fix scrolling on initial load
+var scroll_reduction_factor = 5 //20% scroll speed
 
-function closeTo(input, target, precision){
-    if (Math.abs(target - input) <= precision){
-        return true
-    }
-    return false
-}
-
-function horizontalScrollOnly(e){
-    document.documentElement.scrollLeft += e.deltaY; //scroll left if scrolling down
-    document.documentElement.scrollLeft += e.deltaX; //scroll left if scrolling left
+function horizontalScrollOnly(e) {
+    document.documentElement.scrollLeft += e.deltaY/scroll_reduction_factor; //scroll left if scrolling down]
     document.body.style.overflowX = 'auto' //allow horizontal scroll
     document.body.style.overflowY = 'hidden' //disallow vertical scroll
 }
 
-function verticalScrollOnly(){
+window.onload = function () {
+    document.body.style.overflowX = 'auto' //allow horizontal scroll
+    document.body.style.overflowY = 'hidden' //disallow vertical scroll
+}
+
+function verticalScrollOnly(e) {
+    document.documentElement.scrollTop += e.deltaX/scroll_reduction_factor; //scroll down if scrolling left
+    document.documentElement.scrollTop += e.deltaY/scroll_reduction_factor; //scroll down if scrolling down
     document.body.style.overflowX = 'hidden' //disallow horizontal scroll
-    document.body.style.overflowY = 'auto' //allow vertical scroll
+    document.body.style.overflowY = 'hidden' //disallow vertical scroll
+}
+
+function invertVerticalScrollOnly(e) {
+    document.documentElement.scrollTop -= e.deltaY/scroll_reduction_factor; //scroll up if scrolling down
+    document.documentElement.scrollTop -= e.deltaX/scroll_reduction_factor; //scroll up if scrolling right
+    document.body.style.overflowX = 'hidden' //disallow horizontal scroll
+    document.body.style.overflowY = 'hidden' //disallow vertical scroll
+}
+
+
+function topRightCorner(e) {
+    console.log('here')
+    document.body.style.overflowX = 'hidden' //disallow horizontal scroll
+    document.body.style.overflowY = 'hidden' //disallow vertical scroll
+    if (e.deltaY < 0) {
+        document.documentElement.scrollLeft += e.deltaY/scroll_reduction_factor; //if scrolling up, scroll left
+    }
+    else if (e.deltaX > 0) {
+        document.documentElement.scrollTop += e.deltaX/scroll_reduction_factor; //if scrolling right, scroll down
+    }
+    else if (e.deltaY > 0) {
+        document.body.style.overflowY = 'auto'
+        // if scrolling up, continue
+    }
+    else if (e.deltaX < 0) {
+        document.body.style.overflowX = 'auto'
+        // if scrolling right, continue
+    }
+}
+
+function bottomLeftCorner(e) {
+    document.body.style.overflowX = 'hidden' //disallow horizontal scroll
+    document.body.style.overflowY = 'hidden' //disallow vertical scroll
+    if (e.deltaY > 0) {
+        document.documentElement.scrollLeft += e.deltaY/scroll_reduction_factor; //if scrolling down, scroll left
+    }
+    else if (e.deltaX < 0) {
+        document.documentElement.scrollTop += e.deltaX/scroll_reduction_factor; //if scrolling left, scroll up
+    }
+    else if (e.deltaY < 0) {
+        document.body.style.overflowY = 'auto'
+        // if scrolling up, continue
+    }
+    else if (e.deltaX > 0) {
+        document.body.style.overflowX = 'auto'
+        // if scrolling right, continue
+    }
+}
+
+function bottomRightCorner(e) {
+    document.body.style.overflowX = 'hidden' //disallow horizontal scroll
+    document.body.style.overflowY = 'hidden' //disallow vertical scroll
+    if (e.deltaY > 0) {
+        document.documentElement.scrollTop -= e.deltaY/scroll_reduction_factor; //if scrolling down, scroll up
+    }
+    else if (e.deltaX > 0) {
+        document.documentElement.scrollTop -= e.deltaX/scroll_reduction_factor; //if scrolling right, scroll up
+    }
+    else if (e.deltaY < 0) {
+        document.documentElement.scrollLeft += e.deltaY/scroll_reduction_factor;
+        // if scrolling up, scroll left
+    }
+    else if (e.deltaX < 0) {
+        document.body.style.overflowX = 'auto'
+        // if scrolling left, continue
+    }
+}
+
+function topLeftCorner(e) {
+    document.body.style.overflowX = 'hidden' //disallow horizontal scroll
+    document.body.style.overflowY = 'hidden' //disallow vertical scroll
+    if (e.deltaY > 0) {
+        document.documentElement.scrollLeft += e.deltaY/scroll_reduction_factor; //if scrolling down, scroll right
+    }
+    else if (e.deltaX < 0) {
+        document.documentElement.scrollTop -= e.deltaX/scroll_reduction_factor; //if scrolling left, scroll down
+    }
+    else if (e.deltaY < 0) {
+        document.documentElement.scrollTop -= 1 * e.deltaY/scroll_reduction_factor;
+        // if scrolling up, scroll down
+    }
+    else if (e.deltaX > 0) {
+        document.body.style.overflowX = 'auto'
+        // if scrolling right, continue
+    }
 }
 
 document.addEventListener('wheel', (event) => {
-    const total_width = document.documentElement.scrollWidth
-    const total_user_scroll_width = window.scrollX + window.innerWidth
-    const total_height = document.documentElement.scrollHeight
-    const total_user_scroll_height = window.scrollY + window.innerHeight
-    
-    if (total_user_scroll_width <= 0.71 * total_width && total_user_scroll_height <= window.innerHeight){
-        // console.log(total_user_scroll_width)
-        // console.log(0.7 * total_width)
-        // console.log('before across')
+    document.body.style.overflowX = 'hidden' //disallow horizontal scroll
+    document.body.style.overflowY = 'hidden' //disallow vertical scroll
+    if (elementIsVisibleInViewport(document.getElementById("0")) ||
+        elementIsVisibleInViewport(document.getElementById("1")) ||
+        elementIsVisibleInViewport(document.getElementById("9")) ||
+        elementIsVisibleInViewport(document.getElementById("10")) ||
+        elementIsVisibleInViewport(document.getElementById("11")) ||
+        elementIsVisibleInViewport(document.getElementById("20")) ||
+        elementIsVisibleInViewport(document.getElementById("21")) ||
+        elementIsVisibleInViewport(document.getElementById("22")) ||
+        elementIsVisibleInViewport(document.getElementById("23")) ||
+        elementIsVisibleInViewport(document.getElementById("30")) ||
+        elementIsVisibleInViewport(document.getElementById("31")) ||
+        elementIsVisibleInViewport(document.getElementById("32")) ||
+        elementIsVisibleInViewport(document.getElementById("33")) ||
+        elementIsVisibleInViewport(document.getElementById("34"))) {
         horizontalScrollOnly(event)
     }
-    else if (total_user_scroll_width >= 0.69 * total_width && total_user_scroll_height >= 0.99*total_height) {
-        // console.log(total_user_scroll_width)
-        // console.log(0.7 * total_width)
-        // console.log('after across')
-        horizontalScrollOnly(event)
+    else if (elementIsVisibleInViewport(document.getElementById("25"))) {
+        verticalScrollOnly(event)
     }
-    else if (closeTo(total_user_scroll_width, 0.7*total_width, 100)) {
-        document.documentElement.scrollLeft += 0.7*total_width - total_user_scroll_width
-        verticalScrollOnly()
-        // console.log(total_user_scroll_width)
-        // console.log(0.7 * total_width)
-        // console.log('down')
+    else if (elementIsVisibleInViewport(document.getElementById("2")) ||
+        elementIsVisibleInViewport(document.getElementById("24")) ||
+        elementIsVisibleInViewport(document.getElementById("35"))) {
+        topRightCorner(event)
     }
-    else{
-        // any scrolling
+    else if (elementIsVisibleInViewport(document.getElementById("3")) ||
+        elementIsVisibleInViewport(document.getElementById("4")) ||
+        elementIsVisibleInViewport(document.getElementById("5")) ||
+        elementIsVisibleInViewport(document.getElementById("6")) ||
+        elementIsVisibleInViewport(document.getElementById("7")) ||
+        elementIsVisibleInViewport(document.getElementById("26")) ||
+        elementIsVisibleInViewport(document.getElementById("27")) ||
+        elementIsVisibleInViewport(document.getElementById("28")) ||
+        elementIsVisibleInViewport(document.getElementById("36")) ||
+        elementIsVisibleInViewport(document.getElementById("37")) ||
+        elementIsVisibleInViewport(document.getElementById("38")) ||
+        elementIsVisibleInViewport(document.getElementById("39")) ||
+        elementIsVisibleInViewport(document.getElementById("40"))) {
+        verticalScrollOnly(event)
+    }
+    else if (elementIsVisibleInViewport(document.getElementById("8")) ||
+        elementIsVisibleInViewport(document.getElementById("29"))) {
+        bottomLeftCorner(event)
+    }
+    else if (elementIsVisibleInViewport(document.getElementById("12"))) {
+        bottomRightCorner(event)
+    }
+    else if (elementIsVisibleInViewport(document.getElementById("13")) ||
+        elementIsVisibleInViewport(document.getElementById("14")) ||
+        elementIsVisibleInViewport(document.getElementById("15")) ||
+        elementIsVisibleInViewport(document.getElementById("16")) ||
+        elementIsVisibleInViewport(document.getElementById("17")) ||
+        elementIsVisibleInViewport(document.getElementById("18"))) {
+        invertVerticalScrollOnly(event)
+    }
+    else if (elementIsVisibleInViewport(document.getElementById("19"))) {
+        topLeftCorner(event)
+    }
+    else {
         document.body.style.overflowX = 'auto'
         document.body.style.overflowY = 'auto'
     }
